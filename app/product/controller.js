9c5148f798112042ad315bab5e83d3a853ddaf05
husnil maarif <husnilmaarif.hm@gmail.com>
2022-03-03 22:47:18 +0800

    task api express mysql

--- app/product/controller.js
+++ app/product/controller.js
@@ -3,18 +3,35 @@ const path = require("path");
 const fs = require("fs");
 
 const index = (req, res) => {
-  connection.query(
-    {
+  const { search } = req.query;
+  let exec = {};
+  if (search) {
+    exec = {
+      sql: "SELECT * FROM products WHERE name LIKE ?",
+      values: [`%${search}%`],
+    };
+  } else {
+    exec = {
       sql: "SELECT * FROM products",
-    },
-    _response(res)
-  );
+    };
+  }
+  connection.query(exec, _response(res));
 };
 
 const view = (req, res) => {
   connection.query(
     {
       sql: "SELECT * FROM products WHERE id = ?",
+      values: [req.params.id],
+    },
+    _response(res)
+  );
+};
+
+const destroy = (req, res) => {
+  connection.query(
+    {
+      sql: "DELETE FROM products WHERE id = ?",
       values: [req.params.id],
     },
     _response(res)
@@ -37,6 +54,23 @@ const store = (req, res) => {
   }
 };
 
+const update = (req, res) => {
+  const { users_id, name, price, stock, status } = req.body;
+  const image = req.file;
+  let sql = "";
+  let values = [];
+  if (image) {
+    const target = path.join(__dirname, "../../uploads", image.originalname);
+    fs.renameSync(image.path, target);
+    sql = "UPDATE products SET users_id = ?, name = ?, price = ?, stock = ?, status = ?, image_url = ? WHERE id = ?";
+    values = [parseInt(users_id), name, price, stock, status, `http://localhost:3000/public/${image.originalname}`, req.params.id];
+  } else {
+    sql = "UPDATE products SET users_id = ?, name = ?, price = ?, stock = ?, status = ? WHERE id = ?";
+    values = [parseInt(users_id), name, price, stock, status, req.params.id];
+  }
+  connection.query({ sql, values }, _response(res));
+};
+
 const _response = (res) => {
   return (error, result) => {
     if (error) {
@@ -53,4 +87,4 @@ const _response = (res) => {
   };
 };
 
-module.exports = { index, view, store };
+module.exports = { index, view, store, update, destroy };
